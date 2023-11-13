516d14d5aae3da27124d8e555a22d2e58296b65d
Daniil Gorbachev <daniilgorbacev20109@yandex.ru>
2023-11-13 15:36:21 +0300

    Первая отправка

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,18 @@
+const cardList = document.querySelector('.places__list');
 // @todo: Темплейт карточки
+
+function addCard (name, link) {
+	const cardTemplate = document.querySelector('#card-template').content;
+	const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+
+	cardElement.querySelector('.card__image').src = link;
+	cardElement.querySelector('.card__title').textContent = name;
+
+	cardList.append(cardElement);
+
+	const deleteButton = cardElement.querySelector('.card__delete-button');
+	deleteButton.addEventListener('click', deleteCard);//Удаление карточки
+}
 
 // @todo: DOM узлы
 
@@ -6,4 +20,13 @@
 
 // @todo: Функция удаления карточки
 
+function deleteCard(evt) {
+	const item = evt.target.closest('.card');
+	item.remove();
+};
+
 // @todo: Вывести карточки на страницу
+
+initialCards.forEach(function(item){
+addCard(item.name, item.link);
+});
